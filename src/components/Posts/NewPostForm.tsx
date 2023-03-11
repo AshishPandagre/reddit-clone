375b2323adaf9d608c2491c4c0fb209471a25fbe
AshishPandagre <ashishpandagre9@gmail.com>
2023-03-11 10:19:24 +0530

    completed post & image upload

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -1,4 +1,16 @@
+import { Post } from "@/src/atoms/postsAtom";
+import { firestore, storage } from "@/src/firebase/clientApp";
 import { Flex, Icon } from "@chakra-ui/react";
+import { User } from "firebase/auth";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  Timestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
+import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { BiPoll } from "react-icons/bi";
 import { BsLink45Deg, BsMic } from "react-icons/bs";
@@ -7,7 +19,9 @@ import ImageUpload from "./PostForm/ImageUpload";
 import TextInputs from "./PostForm/TextInputs";
 import TabItem from "./TabItem";
 
-type NewPostFormProps = {};
+type NewPostFormProps = {
+  user?: User;
+};
 
 const formTabs: TabItemType[] = [
   {
@@ -37,7 +51,8 @@ export type TabItemType = {
   icon: typeof Icon.arguments;
 };
 
-const NewPostForm: React.FC<NewPostFormProps> = () => {
+const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
+  const router = useRouter();
   const [selectedTab, setSelectedTab] = useState(formTabs[0].title);
   const [textInputs, setTextInputs] = useState({
     title: "",
@@ -46,7 +61,48 @@ const NewPostForm: React.FC<NewPostFormProps> = () => {
   const [selectedFile, setSelectedFile] = useState<string>();
   const [loading, setLoading] = useState(false);
 
-  const handleCreatePost = async () => {};
+  const handleCreatePost = async () => {
+    // create new post object
+    // store post in db
+    // check for selectedFile
+    // if yes, store it in storage database
+    // update doc by adding imageURL
+    // redirect user back to community using router
+
+    const { communityId } = router.query;
+    const newPost: Post = {
+      communityId: communityId as string,
+      creatorId: user?.uid as string,
+      creatorDisplayName: user?.email!.split("@")[0] as string,
+      title: textInputs.title,
+      body: textInputs.body,
+      numberOfComments: 0,
+      voteStatus: 0,
+      createdAt: serverTimestamp() as Timestamp,
+    };
+
+    setLoading(true);
+
+    try {
+      const postDocRef = await addDoc(collection(firestore, "posts"), newPost);
+
+      if (selectedFile) {
+        const imageRef = ref(storage, `posts/${postDocRef.id}/image`);
+        await uploadString(imageRef, selectedFile, "data_url");
+        const downloadURL = await getDownloadURL(imageRef);
+
+        await updateDoc(postDocRef, {
+          imageURL: downloadURL,
+        });
+      }
+    } catch (err: any) {
+      console.log("handleCreatePost : ", err.message);
+    }
+
+    setLoading(false);
+
+    // router.back()
+  };
 
   const onSelectImage = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const reader = new FileReader();
